3b8a0912ea2145607a3f1f69c0271c6c8c2dba54
Stiven-Richardy <stivenrichardy445@gmail.com>
2025-06-15 00:36:41 -0300

    Pequena alteração na exibição.

--- TP04.java
+++ TP04.java
@@ -8,25 +8,24 @@ INTEGRANTES:
 
 public class TP04 {
 	public static void main (String [] args) {
-            // Instâncias da classe Data
-            Data data = new Data();
+        // Instâncias da classe Data
+        Data data = new Data();
 	    int[] dataParams = {16,8,2025}; 
 	    Data data2 = new Data(dataParams[0], dataParams[1], dataParams[2]);
 
-            data.mostra1();
-            System.out.println(data.mostra2());
-	    System.out.println("--------------------");
-            System.out.println(data.bissexto()? "Eh ano bissexto" : "Nao eh ano bissexto");
-	    System.out.println("--------------------");
-            System.out.println(data.diasTranscorridos() + " dia(s) do ano.");
-	    
-            data2.mostra1();
-            System.out.println(data2.mostra2());
-	    System.out.println("--------------------");
-            System.out.println(data2.bissexto()? "Eh ano bissexto" : "Nao eh ano bissexto.");
-	    System.out.println("--------------------");
-            System.out.println(data2.diasTranscorridos() + " dia(s) do ano.");
-            System.out.println("--------------------");
-            data.apresentaDataAtual();
+        data.mostra1();
+        System.out.println(data.mostra2());
+        System.out.println(data.bissexto()? "Eh ano bissexto" : "Nao eh ano bissexto");
+        System.out.println(data.diasTranscorridos() + " dia(s) do ano.");
+        System.out.println("--------------------");
+
+        System.out.println("Data com passagem de parametros:");
+        data2.mostra1();
+        System.out.println(data2.mostra2());
+        System.out.println(data2.bissexto()? "Eh ano bissexto" : "Nao eh ano bissexto.");
+        System.out.println(data2.diasTranscorridos() + " dia(s) do ano.");
+        System.out.println("--------------------");
+        data.apresentaDataAtual();
+        System.out.println("--------------------");
     }
 }
